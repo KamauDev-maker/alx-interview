fa02d1211ed1131b122eb11727d79679145efb2a
KamauDev-maker <oscarnjenga@gmail.com>
2023-11-23 14:47:54 -0500

    making change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Making change
+"""
+
+
+def makeChange(coins, total):
+    """
+    determine the fewest number of coins needed to meet a given amount
+    """
+    if total <= 0:
+        return 0
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+    return dp[total] if dp[total] != float('inf') else -1
